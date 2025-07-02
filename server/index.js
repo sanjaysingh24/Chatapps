072373df5916ccd3c29d3ffbcfe1ca055e3affad
sanjay <89508377+sanjaysingh24@users.noreply.github.com>
2025-07-02 21:03:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,8 +7,7 @@ import { connectDb } from './db/connect.js';
 import { initializeSocket } from './socket/index.js';
 import cookieParser from "cookie-parser";
 dotenv.config();
-//update commits
-//today commit
+//new commits
 const app = express();
 const PORT = process.env.PORT || 3000;
 
