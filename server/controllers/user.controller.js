fe060e055ef07ea1d54714019e45d86f643bb544
sanjaysingh24 <sanjubora84@gmail.com>
2025-07-13 12:31:15 +0530

    update

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -58,6 +58,12 @@ export const getmessages = async (req, res) => {
 
 
 export const creategroup = async(req,res)=>{
- 
+ try{
+    
+
+ }catch(err){
+  console.log(err);
+  return res.status(500).json({ message: 'Server error', isSuccess: false });
+ }
 
 }
